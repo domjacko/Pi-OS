b86b8fb5c815426bb88f3d8baec648bc12d467f7
Dom Jackson <domjackson94@gmail.com>
2015-01-17 01:11:07 +0000

    Completed Tutorial 4
    Implemented new wait function using Raspberry Pi Timer hardware

--- source/main.s
+++ source/main.s
@@ -1,94 +1,90 @@
-/*******************************************************************************
-* main.s
-* Contains main operating system
-* Dom Jackson's Pi OS
-* @domjacko
-*******************************************************************************/
+/******************************************************************************
+*	main.s
+*	 by Alex Chadwick
+*
+*	A sample assembly code implementation of the ok04 operating system, that 
+*	simply turns the OK LED on and off repeatedly, synchronising using the 
+*	system timer.
+*	Sections changed since ok03.s are marked with NEW.
+*
+*	main.s contains the main operating system, and IVT code.
+******************************************************************************/
 
 /*
-* Instructions for Assembler
-* Tells Linker to put code in .init section which is at start of output
-* Stops toolchain getting upset by defining a _start point even though
-* it is unnecessary to writing an OS as _start is always whatever comes
-* first which we set up with .section .init
+* .globl is a directive to our assembler, that tells it to export this symbol
+* to the elf file. Convention dictates that the symbol _start is used for the 
+* entry point, so this all has the net effect of setting the entry point here.
+* Ultimately, this is useless as the elf itself is not used in the final 
+* result, and so the entry point really doesn't matter, but it aids clarity,
+* allows simulators to run the elf, and also stops us getting a linker warning
+* about having no entry point. 
 */
 .section .init
 .globl _start
 _start:
 
+/*
+* Branch to the actual main code.
+*/
 b main
 
-/* 
-* Tells the assembler to put this code with the rest.
+/*
+* This command tells the assembler to put this code with the rest.
 */
 .section .text
 
 /*
-* Main method
+* main is what we shall call our main operating system method. It never 
+* returns, and takes no parameters.
+* C++ Signature: void main(void)
 */
 main:
-	mov sp,#0x8000
 
-	/* 
-	* Use SetGpioFunction function from gpio.s to set the function
-	* of GPIO port 16 (OK LED) to 001 (binary)
-	*/
-	pinNum .req r0
-	pinFunc .req r1
-	mov pinNum,#16
-	mov pinFunc,#1
-	bl SetGpioFunction
-	.unreq pinNum
-	.unreq pinFunc
+/*
+* Set the stack point to 0x8000.
+*/
+mov sp,#0x8000
 
-	/*
-	* Use SetGpio function from gpio.s to set GPIO 16 to low,
-	* causing the LED to turn on.
-	*/
-	loop$:
-		pinNum .req r0
-		pinVal .req r1
-		mov pinNum,#16
-		mov pinVal,#0
-		bl SetGpio
-		.unreq pinNum
-		.unreq pinVal
+/*
+* Use our new SetGpioFunction function to set the function of GPIO port 16 (OK 
+* LED) to 001 (binary)
+*/
+mov r0,#16
+mov r1,#1
+bl SetGpioFunction
 
-		/*
-		* Decrement down from 3F0000 to 0 to act as a delay
-		*/
-		decr .req r0
-		mov decr,#0x3F0000
-		wait1$: 
-			sub decr,#1
-			teq decr,#0
-			bne wait1$
-		.unreq decr
+/*
+* Use our new SetGpio function to set GPIO 16 to low, causing the LED to turn 
+* on.
+*/
+loop$:
+mov r0,#16
+mov r1,#0
+bl SetGpio
 
-		/*
-		* Use SetGpio function from gpio.s to set GPIO 16 to high,
-		* causing the LED to turn on.
-		*/
-		pinNum .req r0
-		pinVal .req r1
-		mov pinNum,#16
-		mov pinVal,#1
-		bl SetGpio
-		.unreq pinNum
-		.unreq pinVal
+/* NEW
+* We wait using our new method. We use a value of 100000 micro seconds for the
+* delay causing the light to flash 5 times per second.
+*/
+ldr r0,=100000
+bl Wait
 
-		/*
-		* Delay a second time
-		*/
-		decr .req r0
-		mov decr,#0x3F0000
-		wait2$:
-			sub decr,#1
-			teq decr,#0
-			bne wait2$
-		.unreq decr
+/*
+* Use our new SetGpio function to set GPIO 16 to high, causing the LED to turn 
+* on.
+*/
+mov r0,#16
+mov r1,#1
+bl SetGpio
 
-	/*
-	* Loop over the code continuously
-	*/
-	b loop$
+/* NEW
+* We wait using our new method. We use a value of 100000 micro seconds for the
+* delay causing the light to flash 5 times per second.
+*/
+ldr r0,=100000
+bl Wait
+
+/*
+* Loop over this process forevermore
+*/
+b loop$
